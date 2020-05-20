6b75828244bba5db3d18556eeb1a0ec21a09873d
Tomasz Pakula <tomasz.pakula@huuugegames.com>
2020-05-20 23:32:12 +0200

    External Systems.

---
+++ src/main/java/com/vendor/externalsystems/Lights.java
@@ -0,0 +1,18 @@
+package com.vendor.externalsystems;
+
+public class Lights {
+
+    int position;
+
+    /**
+     * null - brak opcji w samochodzie
+     * 1-3 - w dół
+     * 7-10 - w górę
+     *
+     * @return
+     */
+    public Integer getLightsPosition() {
+        return position;
+    }
+
+}
