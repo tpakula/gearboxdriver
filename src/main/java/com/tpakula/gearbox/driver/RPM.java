e7924a65ab7965328bbf6aa3eafc7d2b3e68be26
Tomasz Pakula <tomasz.pakula@huuugegames.com>
2020-05-20 23:46:26 +0200

    Added missing equals and hashcode

--- src/main/java/com/tpakula/gearbox/driver/RPM.java
+++ src/main/java/com/tpakula/gearbox/driver/RPM.java
@@ -1,5 +1,7 @@
 package com.tpakula.gearbox.driver;
 
+import java.util.Objects;
+
 public class RPM implements Comparable<RPM> {
     private long rpm;
 
@@ -25,5 +27,25 @@ public class RPM implements Comparable<RPM> {
             throw new IllegalArgumentException("Rpm should be positive");
         }
         return new RPM(rpm);
+    }
+
+    @Override
+    public String toString() {
+        return "RPM{" +
+                "rpm=" + rpm +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RPM rpm1 = (RPM) o;
+        return rpm == rpm1.rpm;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rpm);
     }
 }
