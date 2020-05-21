1740867ed41a58a5cb078d939c7c45abd493199a
Tomasz Pakula <tomasz.pakula@huuugegames.com>
2020-05-21 23:54:14 +0200

    Implementation of Gearbox driver

--- src/main/java/com/tpakula/gearbox/driver/GearboxDriver.java
+++ src/main/java/com/tpakula/gearbox/driver/GearboxDriver.java
@@ -1,32 +1,48 @@
 package com.tpakula.gearbox.driver;
 
 import com.vendor.externalsystems.ExternalSystems;
-import com.vendor.externalsystems.Gearbox;
 
 public class GearboxDriver {
     private Object[] characteristics = new Object[]{2000d, 1000d, 1000d, 0.5d, 2500d, 4500d, 1500d, 0.5d, 5000d, 0.7d, 5000d, 5000d, 1500d, 2000d, 3000d, 6500d, 14d};
+
+    private static final int MIN_GEAR = 1;
+    private static final int MAX_GEAR = 8;
+
 
     enum GearboxState {
         NEUTRAL,
         PARK,
         REVERSE,
-        DRIVE
+        DRIVE;
+
     }
 
-    private static final int MIN_GEAR = 1;
-    private static final int MAX_GEAR = 8;
-
-    private Gearbox gearbox;
+    private final GearboxAdapter gearbox;
+    private final GearCalculator gearCalculator;
+    private GearboxState state = GearboxState.PARK;
     private ExternalSystems externalSystems;
 
-    private GearboxState state = GearboxState.PARK;
 
-    private GearCalculator gearCalculator;
+    public GearboxDriver(GearboxAdapter gearbox, GearCalculator gearCalculator) {
+        this.gearbox = gearbox;
+        this.gearCalculator = gearCalculator;
+    }
+
 
     public void updateGear() {
         if (state == GearboxState.DRIVE) {
-            externalSystems.getCurrentRpm();
+            RPM currentRpm = getRpm();
+            Gear currentGear = gearbox.getCurrentGear();
+
+            Gear gear = gearCalculator.calulateGear(currentRpm, currentGear);
+
+            gearbox.setGear(gear);
         }
+    }
+
+    private RPM getRpm() {
+        double currentRpm = externalSystems.getCurrentRpm();
+        return RPM.create(Math.round(currentRpm));
     }
 
     private GearCalculator createEcoGearCalculator(int minRpmIndex, int maxRpmIndex) {
